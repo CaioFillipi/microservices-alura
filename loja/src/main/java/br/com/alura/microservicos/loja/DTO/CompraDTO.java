dd1bdce06bb95dc4dcf91c15522ad4fd0e3ef944
Caio Fillipi <caio.fillipi@outlook.com>
2022-03-13 15:29:42 -0300

    Commit inicial

---
+++ loja/src/main/java/br/com/alura/microservicos/loja/DTO/CompraDTO.java
@@ -0,0 +1,9 @@
+package br.com.alura.microservicos.loja.DTO;
+
+import java.util.List;
+
+public class CompraDTO {
+
+    private List<ItemDTO> itens;
+    private EnderecoDTO endereco;
+}
---
+++ loja/src/main/java/br/com/alura/microservicos/loja/DTO/EnderecoDTO.java
@@ -0,0 +1,8 @@
+package br.com.alura.microservicos.loja.DTO;
+
+public class EnderecoDTO {
+
+    private String rua;
+    private String numero;
+    private String estado;
+}
